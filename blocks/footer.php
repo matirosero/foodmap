f539d82d44a813dcd2fd1c6b419361361be9d182
MatiRosero <gekidasa@gmail.com>
2014-07-07 21:45:36 +0200

    footer links

--- blocks/footer.php
+++ blocks/footer.php
@@ -3,22 +3,20 @@
 
 		<!-- #footer-content -->
 		<div id="footer-content">
-				&copy; FoodMapp 2014<br />
 
+
+			<nav id="footer-menu">
+				<a href="index.php" class="footer-menu-home"><?php include('includes/logo-foodmap-footer.svg');?></a>
+				<a href="http://facebook.com" class="footer-menu-social"><?php include('includes/icon-footer-fb.svg');?></a>
+				<a href="http://twitter.com" class="footer-menu-social"><?php include('includes/icon-footer-twitter.svg');?></a>
+				<a href="mailto:info@foodmap.es" class="footer-menu-social"><?php include('includes/icon-footer-email.svg');?></a>
+			</nav>
 			<p>
 				<strong>elBulli</strong>foundation | <strong>Bulli</strong>Pedia
 			</p>
 
-			<nav class="footer-menu">
-				<ul>
-					<li><a href="">Contacto</a></li>
-					<li><a href="">FAQs</a></li>
-					<li><a href="">Mapa del sitio</a></li>
-					<li><a href="">Términos y condiciones</a></li>
-					<li><a href="">Política de privacidad </a></li>
-					<li><a href="">Cookies</a></li>
-
-				</ul>
+			<nav class="footer-menu-small">
+				<a href="">Contacto</a> | <a href="">FAQs</a> | <a href="">Mapa del sitio</a> | <a href="">Términos y condiciones</a> | <a href="">Política de privacidad </a> | <a href="">Cookies</a>
 			</nav>
 		</div><!-- /#footer-content -->
 
