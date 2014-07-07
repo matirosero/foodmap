58340c489d9a0742447a473879f77f9e715eb2fd
MatiRosero <gekidasa@gmail.com>
2014-07-07 18:33:48 +0200

    footer icons

--- blocks/footer.php
+++ blocks/footer.php
@@ -3,10 +3,23 @@
 
 		<!-- #footer-content -->
 		<div id="footer-content">
-			<p>
 				&copy; FoodMapp 2014<br />
+
+			<p>
 				<strong>elBulli</strong>foundation | <strong>Bulli</strong>Pedia
 			</p>
+
+			<nav class="footer-menu">
+				<ul>
+					<li><a href="">Contacto</a></li>
+					<li><a href="">FAQs</a></li>
+					<li><a href="">Mapa del sitio</a></li>
+					<li><a href="">Términos y condiciones</a></li>
+					<li><a href="">Política de privacidad </a></li>
+					<li><a href="">Cookies</a></li>
+
+				</ul>
+			</nav>
 		</div><!-- /#footer-content -->
 
 	</footer><!-- /#page-footer -->
