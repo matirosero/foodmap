65993c0787cb70746e3901c1bd3c75e8c374854d
MatiRosero <gekidasa@gmail.com>
2014-07-15 17:42:08 +0200

    force fullscreen on ios

--- blocks/header.php
+++ blocks/header.php
@@ -8,7 +8,7 @@
 
 	<title>FoodMap</title>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
 
 
 
