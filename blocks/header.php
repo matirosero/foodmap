992bbc14ecf184c21e23ac38961e929af1d1a621
MatiRosero <gekidasa@gmail.com>
2014-07-16 15:31:30 +0200

    user profile

--- blocks/header.php
+++ blocks/header.php
@@ -17,6 +17,7 @@
 	<link rel="stylesheet" href="http://necolas.github.io/normalize.css/3.0.1/normalize.css" type="text/css">
 	<link rel="stylesheet" href="style.css" type="text/css">
 	<link rel="stylesheet" href="sidepanel.css" type="text/css">
+	<link rel="stylesheet" href="user-style.css" type="text/css">
 	<link href="media-queries.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="ammap/ammap.css" type="text/css">
 	<!-- <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" /> -->
--- blocks/user.php
+++ blocks/user.php
@@ -2,7 +2,126 @@
 
 
 
-<h2>usuario</h2>
-<p>*Insistently* Bow ties are cool! Come on Amy, I'm a normal bloke, tell me what normal blokes do!<br /><i>The Doctor</i></p>
-<p>Did I mention we have comfy chairs?<br /><i>The Doctor</i></p><p>No&hellip; It's a thing; it's like a plan, but with more greatness.<br /><i>The Doctor</i></p><p>I'm the Doctor. Well, they call me the Doctor. I don't know why. I call me the Doctor too. I still don't know why.<br /><i>The Doctor</i></p>
+<!-- CODIGO DE PAGINA PERFIL USUARIO EMPIEZA AQUI -->
+
+<div class="user-content">
+	<div id="user-header">
+		<div id="user-thumb">
+			<img src="images/user-thumb.jpg" alt="Mi foto" height="180" width="180">
+		</div>
+		<div id="welcome">
+			<h2>¡Hola, Álvaro! <br/ > En tu maleta llevas:</h2>
+		</div>
+	</div>
+	<div id="user-actions">
+
+		<div id="user-menu">
+			<h2>MI PERFIL</h2>
+			<h5>EDITAR</h5>
+			<div id="user-menu-content">
+				<ul>
+					<li class="user-menu-active">MIS FOODTRIPS<span style="margin-left:38px">3</span></li>
+					<a href=""><li>MIS PLATOS<span style="margin-left:65px">0</span></li></a>
+					<a href=""><li>MIS INGREDIENTES<span style="margin-left:16px">0</span></li></a>
+				</ul>
+			</div>
+		</div>
+
+		<div id="user-main-header">
+			<h2><strong>MIS FOOD</strong>TRIPS </h2>
+		</div>
+
+		<div id="user-main-content">
+			<ul>
+				<li>
+					<div class="user-foodtrip-new">
+						<div class="user-foodtrip-image">
+							<img src="images/user-new-foodtrip-thumb.jpg" alt="Nuevo FoodTrip">
+						</div>
+
+						<div class="user-foodtrip-description">
+							<h2><strong>CREAR NUEVO FOOD</strong>TRIP</h2>
+						</div>
+						<div id="user-add-foodtrip">
+							<a href=""><img src="images/add-foodtrip.png" alt="Nuevo FoodTrip" height="26" width="26"></a>
+						</div>
+					</div>
+				</li>
+				<h3><strong>FOOD</strong>TRIPS<strong> PREMIUM</strong>
+				<span class="user-tips">¡Te quedan 4 FoodTrips <span style="color: #eb4b3f"> GRATIS!</span></span>
+				</h3>
+				<li>
+					<div class="user-foodtrip-item">
+						<div class="user-foodtrip-image">
+							<img src="images/foodtrip-elbulli-thumb.png" alt="ElBulli Costa Brava" height="140" width="140">
+						</div>
+						<div class="user-foodtrip-description">
+							<h2><strong>elBulli</strong> Costa Brava</h2>
+							<p><strong>CONTENIDO</strong> Creado por La BulliFoundation <br/ >
+								Explicaciones de Ferrán Adrià <br />
+								15 platos locales con recetas de ElBulli<br />
+								20 productos locales<br />
+							</p>
+						</div>
+						<div class="user-foodtrip-button">
+							<a href=""><img src="images/user-foodtrip-buttons.png" alt=""></a>
+						</div>
+					</div>
+				</li>
+					<h3><strong>MIS FOOD</strong>TRIPS</h3>
+				<li>
+					<div class="user-foodtrip-item">
+						<div class="user-foodtrip-image">
+							<img src="images/user-india-thumb.jpg" alt="Sur de India" height="140" width="140">
+						</div>
+						<div class="user-foodtrip-description">
+							<h2><strong>SUR DE INDIA</strong><h2>
+							<p><strong>CONTENIDO</strong> Creado por FoodMap y <br/ >editado por mí <br/ >
+								6 Regiones <br />
+								20 platos  <br />
+								10 productos <br />
+							</p>
+						</div>
+						<div class="user-foodtrip-button">
+							<a href=""><img src="images/user-foodtrip-buttons-off.png" alt=""></a>
+						</div>
+					</div>
+				</li>
+				<p></p>
+				<li>
+					<div class="user-foodtrip-item">
+						<div class="user-foodtrip-image">
+							<img src="images/user-japon-thumb.jpg" alt="Japón" height="140" width="140">
+						</div>
+						<div class="user-foodtrip-description">
+							<h2><strong>JAPÓN</strong></h2>
+							<p><strong>CONTENIDO</strong> Creado por mí <br/ >
+							3 Regiones <br />
+							15 platos  <br />
+							10 productos <br />
+							</p>
+						</div>
+						<div class="user-foodtrip-button">
+							<a href=""><img src="images/user-foodtrip-buttons-off.png" alt=""></a>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+
+	</div>
+</div>
+
+
+
+
+
+
+
+<!-- CODIGO DE PAGINA PERFIL USUARIO TERMINA AQUI -->
+
+
+
+
+
 		</div>
