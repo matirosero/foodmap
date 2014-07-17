bd0c57301bd8941d8bba9da081789f548e5f7e94
MatiRosero <gekidasa@gmail.com>
2014-07-17 02:27:33 +0200

    plans

---
+++ blocks/plans.php
@@ -0,0 +1,75 @@
+		<div id="plans" class="global-container">
+
+			<div class="plans-header center">
+				<h2>elige tu  plan personalizado</h2>
+				<p>
+					Puedes registrarte gratis, ser usuario premium con más vantajas o compar el FoodTrip Especial que te interese.
+Disfruta de las ventajas de nuestras diferentes licencias, tú decides!
+				</p>
+			</div>
+
+			<div class="plans-box clear">
+				<div class="pad10 clear">
+
+					<div class="plan-user plan-free">
+						<div class="pad10">
+							<h3>
+								<span class="light">Usuario</span><br />
+								gratuito
+							</h3>
+
+							<h4 class="red">Este plan<br /> está limitado a:</h4>
+
+							<p class="center">
+						Guardar platos /productos<br />
+Descargar en PDF la información<br />
+Consultar Foodtrips<br />
+Crear un sólo Foodtrip<br />
+Sin acceso a información offline<br />
+Sin acceso Foodtrips especiales<br /><br />
+							</p>
+
+							<button class="redbtn">REGÍSTRATE GRATIS</button>
+						</div>
+					</div>
+
+					<div class="plan-user plan-premium">
+						<div class="pad10">
+							<h3>
+								<span class="light">Usuario</span><br />
+								gratuito
+							</h3>
+
+							<h4 class="red"><span class="x2">2</span> €/mes <span class="light">(24 €/año)</span></h4>
+
+							<p class="center">
+						Guardar platos /productos<br />
+Descargar en PDF la información<br />
+Consultar y editar Foodtrips<br />
+Crear los Foodtrips que quiereas<br />
+Descarga de información offline<br />
+Acceso a todos los Foodtrips especiales<br />
+							</p>
+
+							<button class="redbtn">LO QUIERO</button>
+						</div>
+					</div>
+				<div class="plan-foodtrip">
+					<div class="pad10">
+						<h3>
+							1 food<span class="light">trip</span> premium
+						</h3>
+
+						<p>Creado por expertos del mundo de la cocina.
+							Con lugares, platos y recomendaciones de sus propios viajes.
+						</p>
+						<button class="redbtn">5 € / food<span class="light">trip</span> premium</button>
+					</div>
+				</div>
+
+			</div></div>
+
+
+
+
+		</div>
